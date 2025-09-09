8c03571eb49f7682b0819bcf7c601c9072b97936
Dasari Harsha Vardhan <152957034+harsha79937@users.noreply.github.com>
2025-09-09 10:53:25 +0530

    Add files via upload

---
+++ matrix/(2x3)x(3x2).js
@@ -0,0 +1,20 @@
+let m1=[
+    [3,4,5],
+    [6,7,8]
+];
+let m2=[
+    [1,2],
+    [3,4],
+    [5,6]
+];
+let m3=[];
+for(let i=0;i<m1.length;i++){
+    m3[i]=[];
+    for(let j=0;j<m2[0].length;j++){
+        m3[i][j]=0;
+        for(let k=0;k<m1[0].length;k++){
+            m3[i][j]+=m1[i][k]*m2[k][j];      
+        }
+    }
+}
+       
